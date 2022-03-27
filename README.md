100174fe4b897a8ccbb2f0e1ad9817092cd6a6ff
teaishealthy <teaishealthy@protonmail.com>
2022-03-27 14:47:17 +0200

    feat: Initial Commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Frame Extractor
+
+![](https://img.shields.io/badge/code%20style-black-black?style=for-the-badge) ![](https://img.shields.io/badge/python-3.8%2B-blue?style=for-the-badge) ![](https://img.shields.io/codefactor/grade/github/teaishealthy/frame_extractor?style=for-the-badge)
+
+Discord bot that can extract a single frame from a video.
+
+## Features
+
+prefix is one of [nextcord!, nc!, pycord!, pc!].
+
+| Command | Description |
+| ------ | ----------- |
+| prefix!version | Displays the versions of the used dependecies |
+| prefix!frame \<frame:int>  | Extracts the frame from the video and sends it as a file |
+
+## Using 
+
+ - nextcord https://github.com/nextcord/nextcord
+ - pycord https://github.com/alesanfra/pycord 
+
+## Development
+
+* Install requirements.txt
+* Make sure ffmpeg and ffprobe binaries are in your PATH
+* Export your bot token to the environment variable `DISCORD_TOKEN`
+* Run the bot with `python3 main.py`
+
+## Contributing
+
+Contributions are welcome but adhere to the following guidelines
+
+### Adding a dependency
+
+* Make sure it is somewhat related to "discord bot development" and its ecosystem.
+* Add the dependency as a prefix to the bot
+
+Have fun!
+
+## License
+
+Licensed under the MIT License
+
+See [LICENSE](LICENSE)
---
+++ main.py
@@ -0,0 +1,69 @@
+import os
+import shutil
+import tempfile
+import traceback
+from importlib.metadata import version
+from pathlib import Path
+
+import aiohttp
+import nextcord
+from nextcord.ext import commands
+from nextcord.ext.commands import Bot
+from PIL import Image
+from pycord import video_reader
+
+bot = Bot(["pycord!", "pc!", "nextcord!", "nc!"])
+
+
+@bot.command("frame")
+async def convert_command(ctx: commands.Context, frame: int = 0):
+    if not ctx.message.attachments:
+        return await ctx.send("No attachment found")
+
+    temp_dir = Path(tempfile.mkdtemp())
+
+    try:
+        videos = [
+            v
+            for v in ctx.message.attachments
+            if v.content_type and "video" in v.content_type
+        ]
+
+        async with aiohttp.ClientSession() as session:
+            for video in videos:
+                async with session.get(video.url) as response:
+                    with open(temp_dir / video.filename, "wb") as f:
+                        f.write(await response.read())
+
+                    reader = video_reader.VideoReader(temp_dir / video.filename)
+
+                    first_frame = await bot.loop.run_in_executor(
+                        None, lambda: reader.get_batch([frame])
+                    )  # Not sure if this is needed but it is probably a good idea
+
+                    img = Image.frombuffer(
+                        "RGB", (reader.width, reader.height), first_frame.flatten()
+                    )
+                    img.save(temp_dir / f"{video.filename}.png", "png")
+
+                    await ctx.send(
+                        file=nextcord.File(temp_dir / f"{video.filename}.png")
+                    )
+
+    except Exception as e:
+        traceback.print_exception(type(e), e, e.__traceback__)
+
+    shutil.rmtree(temp_dir)
+
+
+@bot.command("version")
+async def info_command(ctx: commands.Context):
+    pycord_version = version("pycord")
+    nextcord_version = version("nextcord")
+
+    await ctx.send(
+        f"pycord version: {pycord_version} and nextcord version: {nextcord_version}"
+    )
+
+
+bot.run(os.environ["DISCORD_TOKEN"])
